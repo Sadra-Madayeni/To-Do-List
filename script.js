6ce36178ef9179e65a9303189fee46882ae35ec8
Sadra-Madayeni <sadra.madayeni@gmail.com>
2024-07-15 19:02:31 +0330

    Add Task Handler

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
- 
+const finishedTasksButton = '<input type="checkbox">';
+
+const inputAddText = document.querySelector('.add-task input');
+
+const addButton = document.querySelector('.button');
+
+const ul = document.querySelector('ul');
+
+function addTasks(e) {
+    if (inputAddText.value.trim() !== '') {
+        const newTaskHTML = `
+            <li>
+                <span class="name">${inputAddText.value}</span>
+                <input type="checkbox">
+            </li>
+        `;
+
+        ul.innerHTML += newTaskHTML;
+
+        inputAddText.value = '';
+    }
+
+    e.preventDefault();
+}
+
+
+addButton.addEventListener('click', addTasks);
+
+
+
