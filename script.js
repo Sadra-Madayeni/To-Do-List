f3416732f0efdc8750774f2c4b05c9ef07847baa
Sadra-Madayeni <sadra.madayeni@gmail.com>
2024-07-17 00:41:10 +0330

    Local Storage Added

--- script.js
+++ script.js
@@ -4,7 +4,47 @@ const inputAddText = document.querySelector('.add-task input');
 
 const addButton = document.querySelector('.button');
 
+
+const finished = document.querySelector('#Checklist #finish');
+
+const notFinished = document.querySelector('#Checklist #notfinished');
+
+
+console.log(notFinished);
+
 const ul = document.querySelector('ul');
+
+
+
+ul.addEventListener('click', Check);
+
+
+let check = false;
+
+function Check(e) {
+    if (e.target.type === 'checkbox') {
+        filterTasks();
+
+    }
+}
+
+
+function filterTasks() {
+    const isFinishedSelected = finished.checked;
+    const isNotFinishedSelected = notFinished.checked;
+
+    const listItems = ul.querySelectorAll('li');
+    listItems.forEach(function (item) {
+        const checkbox = item.querySelector('input[type="checkbox"]');
+        if (isFinishedSelected) {
+            item.style.display = checkbox.checked ? '' : 'none';
+        } else if (isNotFinishedSelected) {
+            item.style.display = checkbox.checked ? 'none' : '';
+        }
+    });
+}
+
+
 
 function addTasks(e) {
     if (inputAddText.value.trim() !== '') {
@@ -17,10 +57,57 @@ function addTasks(e) {
 
         ul.innerHTML += newTaskHTML;
 
+        storeToLocatStorage(inputAddText.value);
+
         inputAddText.value = '';
     }
 
     e.preventDefault();
+}
+
+
+document.addEventListener('DOMContentLoaded', test);
+
+
+
+function test(e) {
+
+    let tasks;
+    if (localStorage.getItem('tasks') === null) {
+        tasks = [];
+    }
+    else {
+        tasks = localStorage.getItem('tasks').split(',');
+    }
+
+    for (let item of tasks) {
+
+        const newTaskHTML = `
+            <li>
+                <span class="name">${item}</span>
+                <input type="checkbox">
+            </li>
+        `;
+
+        ul.innerHTML += newTaskHTML;
+    }
+
+    filterTasks();
+}
+
+
+function storeToLocatStorage(Tasks) {
+    let tasks;
+    if (localStorage.getItem('tasks') === null) {
+        tasks = [];
+    }
+    else {
+        tasks = localStorage.getItem('tasks').split(',');
+    }
+
+    tasks.push(Tasks);
+
+    localStorage.setItem('tasks', tasks);
 }
 
 
